b9de61c30839f313f855373d0b449f08e7630a36
g1eb <mail@g1eb.com>
2017-12-27 16:45:28 +0100

    Render date and time display and placeholder

--- src/datetime-input.component.js
+++ src/datetime-input.component.js
@@ -6,9 +6,46 @@ import styles from './datetime-input.css'
 
 class DatetimeInput extends React.Component {
 
+  renderDate() {
+    let format = this.props.dateFormat || 'DD MMMM YYYY'
+    if ( this.props.datetime ) {
+      return (
+        <div className={styles.date}>
+          {this.props.datetime.format(format)}
+        </div>
+      )
+    }
+  }
+
+  renderTime() {
+    let format = this.props.timeFormat || 'HH : mm : ss'
+    if ( this.props.datetime ) {
+      return (
+        <div className={styles.time}>
+          {this.props.datetime.format(format)}
+        </div>
+      )
+    }
+  }
+
+  renderPlaceholder() {
+    if ( !this.props.datetime ) {
+      return (
+        <div className={styles.placeholder}>
+          {this.props.placeholder}
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className={styles.datetimeInput}>
+        <div className={styles.display}>
+          {this.renderDate()}
+          {this.renderTime()}
+          {this.renderPlaceholder()}
+        </div>
       </div>
     )
   }
