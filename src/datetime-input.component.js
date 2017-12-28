4a215de191b7f71b359e56e70bf0f4a5b0300eb0
g1eb <mail@g1eb.com>
2017-12-28 12:09:47 +0100

    Render initial calendar overview when active

--- src/datetime-input.component.js
+++ src/datetime-input.component.js
@@ -9,13 +9,24 @@ class DatetimeInput extends React.Component {
   constructor(props) {
     super(props)
 
+    this.current = moment()
+
     this.state = {
       active: false,
+      calendar: null,
     }
   }
 
   toggleEditPopover() {
-    this.setState({active: !this.state.active})
+    this.setState({
+      active: !this.state.active,
+    })
+  }
+
+  toggleCalendar() {
+    this.setState({
+      calendar: this.props.datetime.clone() || moment(),
+    })
   }
 
   renderDate() {
@@ -48,13 +59,102 @@ class DatetimeInput extends React.Component {
     }
   }
 
+  renderCalendarHeader() {
+    return (
+      <div className={styles.calendarHeader}>
+        <div className={`${styles.arrow} ${styles.arrowLeft}`}></div>
+        <span>
+          { this.props.datetime.format('YYYY') === this.current.format('YYYY')
+            ? this.props.datetime.format('MMMM')
+            : this.props.datetime.format('MMMM YYYY')
+          }
+        </span>
+        <div className={`${styles.arrow} ${styles.arrowRight}`}></div>
+      </div>
+    )
+  }
+
+  renderWeekdays() {
+    return (
+      <div className={styles.weekdays}>
+        {[0, 1, 2, 3, 4, 5, 6].map(dayIndex => {
+          let date = this.state.calendar.clone()
+          return (
+            <span className={styles.weekday}>
+              {date.startOf('week').add(dayIndex, 'days').format('ddd')}
+            </span>
+          )
+        })}
+      </div>
+    )
+  }
+
+  renderWeeks() {
+    let weekNumberEnd = this.state.calendar.clone().endOf('month').week()
+    let weekNumberStart = this.state.calendar.clone().startOf('month').week()
+    let numWeeks = Math.abs(weekNumberEnd - weekNumberStart + 1)
+
+    return [0, 1, 2, 3, 4, 5].slice(0, numWeeks).map(weekIndex => {
+      return (
+        <div className={styles.week}>
+          {[0, 1, 2, 3, 4, 5, 6].map(dayIndex => {
+            let date = this.state.calendar.clone()
+              .startOf('month')
+              .add(weekIndex, 'weeks')
+              .weekday(dayIndex)
+              .startOf('day')
+
+            let dateStyles = styles.date
+            if ( date.clone().isSame(this.current.clone().startOf('day')) ) {
+              dateStyles += ` ${styles.current}`
+            }
+            if ( date.clone().isSame(this.props.datetime.clone().startOf('day')) ) {
+              dateStyles += ` ${styles.active}`
+            }
+            if ( date.clone().month() !== this.state.calendar.month() ) {
+              dateStyles += ` ${styles.inactive}`
+            }
+
+            return (
+              <span className={dateStyles}>
+                {date.date()}
+              </span>
+            )
+          })}
+        </div>
+      )
+    })
+  }
+
+  renderCalendarBody() {
+    return (
+      <div className={styles.calendarBody}>
+        {this.renderWeekdays()}
+        {this.renderWeeks()}
+      </div>
+    )
+  }
+
+  renderCalendar() {
+    if ( !!this.state.calendar ) {
+      return (
+        <div className={styles.calendar}>
+          {this.renderCalendarHeader()}
+          {this.renderCalendarBody()}
+        </div>
+      )
+    }
+  }
+
   renderEditPopover() {
     if ( this.state.active ) {
       return (
         <div className={styles.editPopover}>
-          <div className={styles.header}>
+          <div className={styles.header}
+            onClick={this.toggleCalendar.bind(this)}>
             {this.props.datetime.format(this.props.dateFormat)}
           </div>
+          {this.renderCalendar()}
         </div>
       )
     }
